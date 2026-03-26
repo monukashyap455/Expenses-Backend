b9bc051706a4268e88a1dedef86f5201ce01e991
monukashyap455 <monukashyap362001@gmail.com>
2026-03-26 21:33:26 +0530

    test

--- routes/expense.js
+++ routes/expense.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const expensesController =require("../controller/expense");
 
+console.log("testinggg...");
+
 router.post('/expenses', expensesController.expensesCreate);
 router.get('/expenses', expensesController.expensesGet);
 router.delete('/expenses/:id', expensesController.expensesDelete);
